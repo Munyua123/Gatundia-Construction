3bb2ad256f3fc53ccb65f81b7a7c251172443d4a
Earl <earlmunyua96@gmail.com>
2024-05-14 12:53:31 +0300

    added google maps

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -133,36 +133,19 @@ function Contact() {
         </div>
       </div>
       <div
-        style={{ width: "23rem", height: "16rem" }}
         className="mx-auto d-block"
       >
         <h2 style={{ textAlign: "center" }}>Google Location</h2>
-        <a
-          href="https://www.google.com/maps/place/Gatundia+Concrete+Crushing+Plant/@0.2288191,36.4592979,15.79z/data=!4m6!3m5!1s0x1787737f83efce35:0x5de2ebcd74987a12!8m2!3d0.227301!4d36.4627878!16s%2Fg%2F11ll9vc1p2?entry=ttu"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="./assets/2024-05-01 (3).png"
-            alt="Google Maps"
-            width="370rem"
-            className="maps mx-auto d-block"
-          />
-        </a>
-        {/* <div className="col-md-5">
-          <div id="tawk_58c1550578d62074c09ca121"></div>
-          <iframe
-            title="Location Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8303457642623!2d36.8225413!3d-1.2750960999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17299c4bddfd%3A0x7546bbe9b1baa6b0!2sBhavesh%20Centre%20-%2001D!5e0!3m2!1sen!2ske!4v1706015132362!5m2!1sen!2ske"
-            width="370rem"
-            height="100%"
-            loading="lazy"
-
-            referrerpolicy="no-referrer-when-downgrade"
-            style={{border: '0px'}}
-            className="maps mx-auto d-block"
-          ></iframe>
-        </div> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d485021.77247428783!2d36.27280135245142!3d0.051421603410170144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1787737f83efce35%3A0x5de2ebcd74987a12!2sGatundia%20Concrete%20Crushing%20Plant!5e0!3m2!1sen!2ske!4v1715679296938!5m2!1sen!2ske"
+          width="400rem"
+          height="230rem"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="maps mx-auto d-block"
+        ></iframe>
       </div>
       <Footer />
     </div>
